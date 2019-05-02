fea17baec5242709967a9d76b62faace8743cdce
JulesBelveze <jules.belveze@hotmail.fr>
2019-05-02 18:44:03 +0200

    add svm parameters tuning function

--- classification/svm_classifier.py
+++ classification/svm_classifier.py
@@ -3,10 +3,12 @@ import numpy as np
 import warnings
 import argparse
 from sklearn.preprocessing import scale
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from sklearn.utils import shuffle
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 from imblearn.over_sampling import SMOTE
 import sys
 
@@ -21,7 +23,7 @@ def oversample_smote(X, y):
 
 
 def main(args):
-    df = pd.read_csv('../data/features_all_nochnnels.csv', index_col=['File', 'Segment'], sep=';')
+    df = pd.read_csv('../data/features_by_channel.csv', index_col=['File', 'Segment'], sep=';')
     accuracy_baseline = df['labels_jules'].value_counts()[0] / sum(df['labels_jules'].value_counts())
 
     Y = df['labels_jules'].values
@@ -38,26 +40,58 @@ def main(args):
 
     X = scale(X)
 
-    K = 5
-    KF = KFold(n_splits=K, shuffle=True)
-    error_test = np.zeros(K)
-    k = 0
-    for train_index, test_index in KF.split(X):
-        X_train = X[train_index, :]
-        Y_train = Y[train_index]
+    print("Baseline accuracy: %".format(accuracy_baseline))
 
-        X_test = X[test_index, :]
-        Y_test = Y[test_index]
+    if args.CV:
+        K = 5
+        KF = KFold(n_splits=K, shuffle=True)
+        error_test = np.zeros(K)
+        error_train = np.zeros(K)
+        k = 0
+        for train_index, test_index in KF.split(X):
+            X_train = X[train_index, :]
+            Y_train = Y[train_index]
+
+            X_test = X[test_index, :]
+            Y_test = Y[test_index]
+
+            clf = SVC()
+            clf.fit(X_train, Y_train)
+
+            Y_hat_train = clf.predict(X_train)
+            Y_hat = clf.predict(X_test)
+
+            error_train[k] = accuracy_score(Y_train, Y_hat_train)
+            error_test[k] = accuracy_score(Y_test, Y_hat)
+
+            k += 1
+
+        error_test = np.mean(error_test)
+        error_train = np.mean(error_train)
+
+        print("Accuracy training: %f" % error_train)
+        print("Accuracy testing: %f" % error_test)
+
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, Y, test_size=0.3, random_state=42)
 
         clf = SVC()
-        clf.fit(X_train, Y_train)
+        clf.fit(X_train, y_train)
 
         Y_hat = clf.predict(X_test)
-        error_test[k] = accuracy_score(Y_test, Y_hat)
 
-        k += 1
+        print(" \n-------------- Test ---------------")
+        print("Accuracy: %f" % accuracy_score(y_test, Y_hat))
+        print(classification_report(y_test, Y_hat, target_names=['bad', 'good']))
 
-    print("Accuracy: %f / baseline: %f" % (np.mean(error_test), accuracy_baseline))
+        plt.figure()
+        sns.heatmap(confusion_matrix(y_test, Y_hat),
+                    annot=True,
+                    cbar=False,
+                    xticklabels=['bad', 'good'],
+                    yticklabels=['bad', 'good'])
+        plt.show()
 
 
 def parse_arguments():
@@ -73,6 +107,12 @@ def parse_arguments():
         type=bool,
         default=False,
         help='Setting to true will upsample the minority class using the SMOTE algorithm'
+    )
+    parser.add_argument(
+        '--CV',
+        type=bool,
+        default=False,
+        help='Setting to true will proceed to a cross-validation'
     )
 
     return parser.parse_intermixed_args()
